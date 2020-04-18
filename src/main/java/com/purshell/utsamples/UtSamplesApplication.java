b3c967c7b32e6fd7c9841438a05e35f313f7699d
purshell <purshell.clx@gmail.com>
2020-04-18 10:19:16 +0800

    init

---
+++ src/main/java/com/purshell/utsamples/UtSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.purshell.utsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purshell/utsamples/UtSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purshell.utsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
